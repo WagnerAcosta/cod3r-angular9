0f439535ddeb937c16db35e21fce159fec335b8a
w2gn3r <w2gn3r@gmail.com>
2020-07-30 14:45:34 -0400

    Atualizando titulo

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private headerService: HeaderService) { }
 
   ngOnInit() {
+  }
+
+
+  public get title(): string {
+    return this.headerService.headerData.title
+  }
+
+  public get icon(): string {
+    return this.headerService.headerData.icon
+  }
+
+  public get routeUrl(): string {
+    return this.headerService.headerData.routeUrl
   }
 
 }
